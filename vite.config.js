261d6087056ea48b2c7617896233ffccaf74825d
TheeFlakes <mmdennis25@gmail.com>
2025-07-26 05:28:48 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		rollupOptions: {
+			external: ['@rollup/rollup-linux-x64-gnu']
+		}
+	},
+	optimizeDeps: {
+		exclude: ['@rollup/rollup-linux-x64-gnu']
+	}
 });
